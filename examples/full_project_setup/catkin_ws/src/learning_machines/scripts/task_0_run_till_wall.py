1265d8f7d3745480e7e2f010d1aa7813d31c072f
andpuc23 <44880821+andpuc23@users.noreply.github.com>
2024-06-07 11:42:00 +0200

    updates to simulations

--- examples/full_project_setup/catkin_ws/src/learning_machines/scripts/task_0_run_till_wall.py
+++ examples/full_project_setup/catkin_ws/src/learning_machines/scripts/task_0_run_till_wall.py
@@ -7,24 +7,35 @@ if __name__ == "__main__":
         rob = HardwareRobobo(camera=True)
     elif sys.argv[1] == "--simulation":
         rob = SimulationRobobo()
+        rob.play_simulation()
     else:
         raise ValueError(f"{sys.argv[1]} is not a valid argument.")
 
     threshold_val = 100 # 10 cm
+      
 
     dist = rob.read_irs()[4] 
     
-    while dist < threshold_val:
-        rob.move(25, 25, 100)
-        # time.sleep(0.01)
+    # while dist < threshold_val:
+    for i in range(1000):
+        rob.move(25, 25, 150)
+        # print('sleeping')
+        # rob.sleep(5)
         dist = rob.read_irs()[4]
         print('proximity:', dist)
+        if dist >= threshold_val:
+            break
         
-    rob.sleep(3)
+    # rob.sleep(3)
+    print('done')
+    # rob.block()
     
-    rob.block()
     rob.move(-25, 25, 1200) # rotate
     
-    rob.block()
+    # rob.block()
+
     rob.move(50, 50, 500) # move again
-    rob.block()
+    # rob.block()
+
+    if isinstance(rob, SimulationRobobo):
+        rob.stop_simulation()
--- examples/full_project_setup/catkin_ws/src/learning_machines/scripts/task_0_test_sensors.py
+++ examples/full_project_setup/catkin_ws/src/learning_machines/scripts/task_0_test_sensors.py
@@ -3,6 +3,16 @@ import sys
 
 from robobo_interface import SimulationRobobo, HardwareRobobo
 if __name__ == "__main__":
-    rob = HardwareRobobo()
-    while True:
-        print(rob.read_irs())
+    if sys.argv[1] == "--hardware":
+        rob = HardwareRobobo(camera=True)
+    elif sys.argv[1] == "--simulation":
+        rob = SimulationRobobo()
+        rob.play_simulation()
+    else:
+        raise ValueError(f"{sys.argv[1]} is not a valid argument.")
+    i = 0
+    while i < 10_000:
+        print('\t'.join([str(x) for x in rob.read_irs()]))
+
+    if isinstance(rob, SimulationRobobo):
+        rob.stop_simulation()
