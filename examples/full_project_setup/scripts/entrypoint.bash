b8b17d667d233e329a7540cd0bf5ce855b9abc62
andpuc23 <44880821+andpuc23@users.noreply.github.com>
2024-06-24 11:44:42 +0200

    upd entrypoint for task 3

--- examples/full_project_setup/scripts/entrypoint.bash
+++ examples/full_project_setup/scripts/entrypoint.bash
@@ -6,4 +6,6 @@ source /root/catkin_ws/setup.bash
 # rosrun learning_machines learning_robobo_controller.py "$@"
 # rosrun learning_machines task_0_run_till_wall.py "$@"
 # rosrun learning_machines task_0_test_sensors.py "$@"
-rosrun learning_machines task_1_avoid_obstacles.py "$@"
+# rosrun learning_machines task_1_avoid_obstacles.py "$@"
+# rosrun learning_machines task_2_old_code.py "$@"
+rosrun learning_machines task_3.py "$@"
