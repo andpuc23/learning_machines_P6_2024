21e4613e58b88c28c1f24d0f9fa88462a714564d
andpuc23 <44880821+andpuc23@users.noreply.github.com>
2024-06-26 17:36:10 +0200

    changed food_thres_val, pixels counting, added irs clipping, phone tilt

--- examples/full_project_setup/catkin_ws/src/learning_machines/scripts/task_3.py
+++ examples/full_project_setup/catkin_ws/src/learning_machines/scripts/task_3.py
@@ -10,7 +10,8 @@ import random
 
 
 action_space = ['move_forward', 'move_backward', 'turn_left', 'turn_right']
-food_threshold_val = 20
+food_threshold_val = 0.02
+rounding_value = 0.05
 
 np.random.seed(0xC0FFEE)
 torch.manual_seed(0xC0FFEE)
@@ -21,14 +22,14 @@ def get_number_of_green_pixels(img) -> float:
     px_num = blue.shape[0]*blue.shape[1]
     mask = (green > blue+10) & (green > red+10)
     count = np.count_nonzero(mask)
-    return count / px_num
+    return count / px_num * 100
 
 def get_number_of_red_pixels(img) -> float:
     blue, green, red = cv2.split(img)
     px_num = blue.shape[0]*blue.shape[1]
     mask = (red > blue+10) & (red > green+10)
     count = np.count_nonzero(mask)
-    return count / px_num
+    return count / px_num * 100
 
 def get_img_sectors_colors(img)->tuple:
     left = img[:img.shape[0]//3, :, :]
@@ -64,7 +65,9 @@ def get_observation(rob) -> list:
     irs = rob.read_irs()
     irs = [irs[6], irs[4], irs[7], irs[5]] # backC, frontC, frontLL, frontRR
     img = rob.get_image_front()
+    cv2.imwrite('/root/results/img.png', img)
     pixels = get_img_sectors_colors(img)
+    # print('get_observation', 'red:', pixels[3:])
     return irs + list(pixels)
 
 def _distance_rob_to_obj(rob, obj) -> float:
@@ -78,23 +81,25 @@ def _distance(rob, obj1, obj2) -> float:
     pos2 = rob._sim.getObjectPosition(obj2, rob._sim.handle_world)
     return np.sqrt((pos1[0]-pos2[0])**2 + (pos1[1] - pos2[1])**2)
 
-def cost_function(rob):
+def cost_function(rob, state):
     distance_rob_to_food = _distance_rob_to_obj(rob, food)
     distance_food_to_base = _distance(rob, food, target)
+    cost = distance_rob_to_food
+    cost += sum(state[:4])*0.01
     if distance_rob_to_food > food_threshold_val:
-        return distance_rob_to_food + 10 # starting dist is 1.325...
+        return cost + 10 # starting dist is 1.325...
     else:
-        return distance_rob_to_food + distance_food_to_base
+        return cost + distance_food_to_base
 
 def train(rob):
-    episode_length = 600
+    episode_length = 300
     learning_rate = 0.1
     discount_factor = 0.9
     epsilon = 1.0
     epsilon_decay = 0.99
     min_epsilon = 0.1
-    episodes_num = 1000
-    q_table_size = 1000
+    episodes_num = 10
+    q_table_size = 10000
     state_shape = 10
 
     q_table = np.zeros((q_table_size, len(action_space)))  # Adjust the state space size as needed
@@ -114,7 +119,7 @@ def train(rob):
             do_action(rob, action, action_space)
             next_state = get_observation(rob)
             next_state_idx = state_to_index(next_state, rounded_states_list)
-            cost = cost_function(rob)
+            cost = cost_function(rob, next_state)
             
             # Update Q-values using cost function
             best_next_action = np.argmin(q_table[next_state_idx, :])
@@ -151,8 +156,10 @@ def _find_nearest(array, value):
     return idx, diff
 
 def state_to_index(state, states_list):
-    rounding_value = 0.05
+    global rounding_value
     rounded_obs = np.array([round(s / rounding_value) * rounding_value for s in state])
+    rounded_obs = np.clip(rounded_obs[:4], 0, 150).tolist() + rounded_obs[4:].tolist()
+    print('rounded:', rounded_obs)
     index, delta = _find_nearest(states_list, rounded_obs)
 
     if np.all(states_list[index] == 0) and np.all(rounded_obs != 0):
@@ -166,7 +173,7 @@ def state_to_index(state, states_list):
     return index
 
 def is_done(state):
-    food_threshold_val = 20
+    global food_threshold_val
     return _distance_rob_to_obj(rob, food) < food_threshold_val
 
 if __name__ == "__main__":
@@ -175,6 +182,7 @@ if __name__ == "__main__":
     elif sys.argv[1] == "--simulation":
         rob = SimulationRobobo()
         rob.play_simulation()
+        rob.set_phone_tilt(110, 50)
     else:
         raise ValueError(f"{sys.argv[1]} is not a valid argument, --simulation or --hardware expected.")
     
